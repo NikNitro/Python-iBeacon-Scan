1acefe196afedaa55164c20ed5e36af2efddda06
NikNitro <adrimlg92@gmail.com>
2017-04-01 19:48:43 +0200

    Now don't save the uid because with mac and rssi we have all we need.

--- blescan.py
+++ blescan.py
@@ -232,8 +232,8 @@ def parse_events_2(sock, macList, loop_count=100):
                     if(packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9]) in macList):
                         Adstring = packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
                         Adstring += ","
-                        Adstring += returnstringpacket(pkt[report_pkt_offset -22: report_pkt_offset - 6]) 
-                        Adstring += ","
+                      #  Adstring += returnstringpacket(pkt[report_pkt_offset -22: report_pkt_offset - 6]) 
+                      #  Adstring += ","
                         Adstring += "%i" % returnnumberpacket(pkt[report_pkt_offset -6: report_pkt_offset - 4]) 
                         Adstring += ","
                         Adstring += "%i" % returnnumberpacket(pkt[report_pkt_offset -4: report_pkt_offset - 2]) 
