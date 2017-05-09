2488d997651329575d07e32c2bdd6a9220b6ec01
NikNitro <adrimlg92@gmail.com>
2017-05-09 13:46:56 +0200

    Fixed any errors. Is necessary to adjust more the MARGEN, NCONSTANT and TXPOWER but now works

--- Posicionar.py
+++ Posicionar.py
@@ -10,15 +10,15 @@ from sympy import *
 # Más info sobre las constantes en la tabla de drive. 
 # Es necesario calibrarlas ante cualquier cambio.
 # Sería interesante automatizar este proceso
-TXPOWER = -72.49269311
-NCONSTANT = 0.3886
+TXPOWER = -69.16666667#-72.49269311
+NCONSTANT = 1#0.3886
 
 def Posicionar(distancias):
 	#POSICIONAR Recibe las distancias a cada baliza en forma de ecuación y
 	# devuelve la posición del elemento a encontrar.
 	#   
 
-
+	MARGEN=100
 
 	## Obtenemos los valores dos a dos y los resolvemos, guardándolos en 
 	# un mapa de resultados
@@ -39,6 +39,11 @@ def Posicionar(distancias):
 				#print(distancias[i])
 				#print(distancias[j])
 				resAux = solve([distancias[i], distancias[j]], [x,y])
+				print('Ecuaciones: \n')
+				print(str(distancias[i]))
+				print(str(distancias[j]))
+				print('Soluciones:\n')
+				print(str(resAux))
 				if(len(resAux)!=0):
 					# Hay que guardar los resultados dependiendo del número de soluciones
 					if(len(resAux)==1):
@@ -61,12 +66,12 @@ def Posicionar(distancias):
 					for k in range(0, len(solx)):
 						""" Vamos a añadir clausulas para ignorar las soluciones complejas """
 						a = complex(solx[k])
-						print("El tipo es: " + str(type(a)))
+						#print("El tipo es: " + str(type(a)))
 						if(a.imag == 0):
 							# INICIO Update 1
 							#if(mapObjX.isKey(single(solx(k)))) 
-							print("solx vale " + str(a))
-							goodKey = IsThereASimilarKey(mapObjX, solx[k]);
+							#print("solx vale " + str(a))
+							goodKey = IsThereASimilarKey(mapObjX, solx[k], MARGEN);
 							#print("Encontrada ke X similar a "+ str(solx[k]) + ': '+str(goodKey)+'\n')
 							if(goodKey!=-1):
 								mapObjX[goodKey] = (mapObjX[goodKey] + 1);
@@ -79,8 +84,8 @@ def Posicionar(distancias):
 						""" Vamos a añadir clausulas para ignorar las soluciones complejas """
 						a = complex(soly[k])
 						if(a.imag == 0):
-							print("soly vale " + str(a))
-							goodKey = IsThereASimilarKey(mapObjY, soly[k]);
+							#print("soly vale " + str(a))
+							goodKey = IsThereASimilarKey(mapObjY, soly[k], MARGEN);
 							#print("Encontrada key Y similar a "+ str(soly[k]) + ': '+str(goodKey)+'\n')
 							if(goodKey!=-1):
 								mapObjY[goodKey] = (mapObjY[goodKey] + 1);
@@ -105,14 +110,14 @@ def Posicionar(distancias):
 	    if(numAux>numX):
 	        numX = numAux;
 	        elemX = elemAux;
-	        print("Nuevo numX = "+ str(elemX))
+	        #print("Nuevo numX = "+ str(elemX))
 	for i in range(1,len(listaY)):
 	    elemAux = listaY[i];
 	    numAux = mapObjY[elemAux];
 	    if(numAux>numY):
 	        numY = numAux;
 	        elemY = elemAux;
-	        print("Nuevo numY = "+ str(elemY))
+	        #print("Nuevo numY = "+ str(elemY))
 
 	print(elemX)
 	print(elemY)
@@ -128,7 +133,7 @@ def rssi2distance(rssi):
 	den = 10*NCONSTANT
 	exp = num/den
 	distance = pow(10, exp)
-	return distance*100 
+	return distance#*100 
 
 
 def IsThereASimilarKey(mapObj, elem, margen=1):
@@ -144,3 +149,17 @@ def average(lista):
 	for n in lista:
 		suma += float(n)
 	return suma/len(lista)
+
+#To use it in virtualScan
+def VirtualBeacons(cual, cuantas):
+	beacons = ['b8:27:eb:33:89:d4', 'b8:27:eb:ba:32:52', 'b8:27:eb:f9:5f:fe']
+	rests   = [',d0:c9:79:93:d8:94,51577,37848,-108,-90',#.67181664', 
+	           ',d0:c9:79:93:d8:94,51577,37848,-108,-94',#.77774606', 
+	           ',d0:c9:79:93:d8:94,51577,37848,-108,-98']#.23123345']
+
+	stri = str(beacons[cual]) + str(rests[cual])
+	li=[]
+	for i in range(cuantas):
+		li +=[stri]
+	print(li)
+	return li
